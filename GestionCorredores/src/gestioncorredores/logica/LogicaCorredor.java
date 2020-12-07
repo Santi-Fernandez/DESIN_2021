51f0d7b788afd2bb5cde6523c569609b2df8a1b8
mad_d <mad_d@192.168.56.1>
2020-12-07 17:52:47 +0100

    Creacion clase corredor Creacion logica corredor - modificar corredor Creacion de corredor - imprimir corredor Creacion clase Carrera Crear corredor y guardado en LogicaCorredor Modificar LogicaCorredor

---
+++ GestionCorredores/src/gestioncorredores/logica/LogicaCorredor.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestioncorredores.logica;
+
+import gestioncorredores.dto.Corredor;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mad_d
+ */
+public class LogicaCorredor {
+    
+    private static List<Corredor> listaCorredores = new ArrayList<Corredor>();
+   
+    
+    public static void anadirCorredor (Corredor corredor) {
+        
+        listaCorredores.add(corredor);
+    }
+
+    public static List<Corredor> getListaCorredores() {
+        return listaCorredores;
+    }
+    
+    
+    
+}
